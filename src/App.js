9cb2ee8e1f4aaacfe64109cdbe7ca5bb238d306b
Sunil Kotian <sunil.kotian@niveussolutions.com>
2025-07-24 00:14:01 +0530

    Switch to BrowserRouter and add Netlify redirects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 
